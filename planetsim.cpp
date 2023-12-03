98646bdddd5510261f7e2a84b1971156ac5acfca
Flw5469 <tonyfong12345@gmail.com>
2023-12-04 00:19:25 +0800

    fixed acceleration not updated

--- planetsim.cpp
+++ planetsim.cpp
@@ -6,20 +6,20 @@ using namespace std;
 #define planetSize 2
 #define tick 1
 #define G 6.647e-11
-#define loopTime 100
+#define loopTime 1
 class Coordinate{
     public: double xCoord;
             double yCoord;
-            void setCoordinate(double xCoord,double yCoord){
-                this->xCoord=xCoord;
-                this->yCoord=yCoord;
+            void setCoordinate(double xInput,double yInput){
+                this->xCoord=xInput;
+                this->yCoord=yInput;
             }
             Coordinate(){
                 this->xCoord=0;
                 this->yCoord=0;
             };
-            Coordinate(double xCoord, double yCoord){
-                setCoordinate(xCoord,yCoord);
+            Coordinate(double xInput, double yInput){
+                setCoordinate(xInput,yInput);
             }
             void add(Coordinate value){
                 xCoord+=value.xCoord;
@@ -40,15 +40,21 @@ class Planet{
             Coordinate acceleration;
             Coordinate velocity;
             int id;
+            Planet(){
+                mass=0;
+                acceleration=Coordinate();
+                velocity=Coordinate();
+                coordinate=Coordinate();
+            };
 
-            Planet(int id,double xCoord, double yCoord,double mass){
-                this->coordinate;
+            Planet(int id,double xCoord, double yCoord,double mass):Planet(){
+                
                 this->id=id;
                 this->coordinate.setCoordinate(xCoord,yCoord);
                 this->mass=mass;
             }
-            Planet(){};
-            Planet(tuple<double,double,double,int> info){
+            
+            Planet(tuple<double,double,double,int> info):Planet(){
                 this->coordinate.setCoordinate(get<0>(info),get<1>(info));
                 this->mass=get<2>(info);
                 this->id=get<3>(info);
@@ -69,46 +75,54 @@ double radius(Planet a,Planet b){
 }
 
 Coordinate distance(Coordinate self, Coordinate target){
-    return Coordinate(target.xCoord-self.xCoord,target.yCoord-target.yCoord);
+    double x=target.xCoord-self.xCoord;
+    double y=target.yCoord-self.yCoord;
+    cout<<"calculating distance"<<x<<" "<<y<<endl;
+    Coordinate distance=Coordinate(target.xCoord-self.xCoord,target.yCoord-self.yCoord);
+    return distance;
 }
 
-void applyAttraction(Planet giver, Planet receiver){
+void applyAttraction(Planet& giver, Planet& receiver){
     Coordinate distanceVector=distance(giver.coordinate,receiver.coordinate);
-    distanceVector.time(G*giver.mass/radius(giver,receiver));
     cout<<"distance vector:";
     distanceVector.display();
+    distanceVector.time(G*giver.mass/radius(giver,receiver));
+    cout<<"scaled distance vector:";
+    distanceVector.display();
     receiver.acceleration.add(distanceVector);
+    cout<<"calculated acceleration: "<<endl;
+    receiver.acceleration.display();
 };
 
-void applyAcceleration(Planet planet){
-    planet.velocity.add(planet.acceleration);
+void applyAcceleration(Planet& planet){
     cout<<"acceleration: "<<endl;
     planet.acceleration.display();
+    planet.velocity.add(planet.acceleration);
 };
-void applyVelocity(Planet planet){
-    planet.coordinate.add(planet.velocity);
+void applyVelocity(Planet& planet){
     cout<<"velocity: "<<endl;
     planet.velocity.display();
+    planet.coordinate.add(planet.velocity);
 };
 
-void update(double inputTick,array<Planet,planetSize> planetList){
-    for (auto receiver:planetList){
-        for (auto giver:planetList){
+void update(double inputTick,array<Planet,planetSize>& planetList){
+    for (auto& receiver:planetList){
+        for (auto& giver:planetList){
             if (receiver.id==giver.id){
                 cout<<"repeated"<<endl;
             }
             else applyAttraction(giver,receiver);
         }
     }
-    for (auto planet:planetList){
+    for (auto& planet:planetList){
+        cout<<endl;
         applyAcceleration(planet);
         applyVelocity(planet);
     }
     render(planetList);
     cout<<endl;
-    
 }
-
+ 
 int main(){
     //x y mass id
    array<tuple<double, double, double, int>, 2> init = {
